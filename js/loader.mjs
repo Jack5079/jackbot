7231f85a2895be8f5d86939a0586b217ad3f18ae
Jack5079 <29169102+Jack5079@users.noreply.github.com>
2019-08-24 14:37:28 -0400

    fix

--- js/loader.mjs
+++ js/loader.mjs
@@ -4,9 +4,9 @@ import { browser } from './users.mjs'
 export default function (commands) {
   document.querySelector('#input').addEventListener('keyup', e => { // For every keypress
     if (e.key === 'Enter') { // If it's enter
-      (m => { // This processes the commmand
-        Object.keys(commands).forEach((n) => { // For every command
-          if (m.content.startsWith(`-${n}`)) commands[n](m, m.content.substring(2 + n.length).split(' ')) // Run the command if input = command (also provide args)
+      (message => { // This processes the commmand
+        Object.keys(commands).forEach((name) => { // For every command
+          if (message.content.startsWith(`-${name}`)) commands[name](message, message.content.substring(2 + name.length).split(' ')) // Run the command if input = command (also provide args)
         })
       })(new Message(e.currentTarget.value, browser))
     }
