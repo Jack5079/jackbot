29a83b3a51cf78daf2111f6b6d4ed7126b05c575
Jack5079 <29169102+Jack5079@users.noreply.github.com>
2019-08-24 14:18:06 -0400

    better

--- main.mjs
+++ main.mjs
@@ -1,21 +1,22 @@
 import { Message } from './message.mjs'
 import { browser } from './users.mjs'
 const commands = { // The command list
-  repeat: async function (message, args) { // Repeats what the user typed after
+  repeat (message, args) { // Repeats what the user typed after
     message.channel.send(args.join(' '))
   },
-  new: async function (message, args) { // Lets users create a new command within the app
+  new (message, args) { // Lets users create a new command within the app
     if (args.length) {
       const name = args[0] // record the name before we remove it
       args.shift() // remove the name
-      this[name] = new (Object.getPrototypeOf(async () => { }).constructor)('message', 'args', args.join(' ')) // make a command with the arguments that are left
+      // eslint-disable-next-line no-new-func
+      this[name] = new Function('message', 'args', args.join(' ')) // make a command with the arguments that are left
       message.channel.send(`🎉Created ${name}!`) // tell the user
     }
   },
-  say: async function (message, args) { // Like -repeat but it hides the message with the command
+  say (message, args) { // Like -repeat but it hides the message with the command
     message.channel.send(args.join(' ')); message.delete()
   },
-  votepoop: async function (message, args) { // I was requested to add this
+  votepoop (message, args) { // I was requested to add this
     message.channel.send('😎 i voted for poop')
   }
 }
