d76c6a05a2ba9376fe23670d3595f850392ff3bf
Jack5079 <29169102+Jack5079@users.noreply.github.com>
2019-08-24 21:06:26 -0400

    Add JSDoc

--- js/loader.mjs
+++ js/loader.mjs
@@ -1,6 +1,21 @@
 import Message from './message.mjs'
 import { browser } from './users.mjs'
-
+/**
+ * Loads commands and settings.
+ * @example <caption>A bot with a say command and the prefix "-"</caption>
+ * import loadCommands from './path/to/loader.mjs'
+ * loadCommands({
+ *    say (message, args) {
+ *      message.channel.send(args.join(' '))
+ *    }
+ *  },
+ *  {
+ *    prefix: '-'
+ * })
+ * @author Jack5079
+ * @param {object} commands An object of functions.
+ * @param {object} options The options that will be used when loading.
+ */
 export default function (commands, options) {
   document.querySelector('#input').addEventListener('keyup', e => { // For every keypress
     if (e.key === 'Enter') { // If it's enter
