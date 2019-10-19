d2cd2bf7d6a9e006e69c615f558864ecdcf79f08
Jack <29169102+Jack5079@users.noreply.github.com>
2019-10-18 20:32:30 -0400

    idk what i did

--- js/users.mjs
+++ js/users.mjs
@@ -1,8 +1,9 @@
+
 export const browser = {
-  name: 'You',
-  url: 'https://yt3.ggpht.com/-7zFDHK5X45w/AAAAAAAAAAI/AAAAAAAAAAA/QJfHeLTEZwE/s900-c-k-no-mo-rj-c0xffffff/photo.jpg'
+  "name": "You",
+  "url": "//yt3.ggpht.com/-7zFDHK5X45w/AAAAAAAAAAI/AAAAAAAAAAA/QJfHeLTEZwE/s900-c-k-no-mo-rj-c0xffffff/photo.jpg"
 }
 export const bot = {
-  name: 'Your computer',
-  url: './images/bot.png'
+  "name": "Your computer",
+  "url": "./images/bot.png"
 }
