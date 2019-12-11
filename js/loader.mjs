e43523edd72f364750c2d707ab853eafafed2106
Jack <29169102+Jack5079@users.noreply.github.com>
2019-12-10 21:50:06 -0500

    standard js

--- js/loader.mjs
+++ js/loader.mjs
@@ -2,7 +2,6 @@
 import Message from './message.mjs'
 
 export default class Loader {
-  deleted = false
   /**
    * Loads commands and settings.
    * @example <caption>A bot with a say command and the prefix "-"</caption>
@@ -22,30 +21,31 @@ export default class Loader {
   constructor (commands, options = {
     prefix: '-'
   }) {
+    this.deleted = false
     document.querySelector('#input').addEventListener('botmessage', event => { // When a message is sent
       if (!this.deleted) {
-      let message = event.detail // Get the message
-      message.reply = content => {
+        const message = event.detail // Get the message
+        message.reply = content => {
         // Add a reply function
-        return new Message(content, options.user) // send msg
-      }
-      Object.keys(commands).forEach(name => {
+          return new Message(content, options.user) // send msg
+        }
+        Object.keys(commands).forEach(name => {
         // For every command
         // example commmand: -test hello
         // example command with spaces: -a test hello
-        if (
-          message.content.startsWith(`${options.prefix}${name} `) // matches any command with a space after
-          || message.content == `${options.prefix}${name}`
+          if (
+            message.content.startsWith(`${options.prefix}${name} `) || // matches any command with a space after
+          message.content === `${options.prefix}${name}`
           ) { // matches commands that are just the command
           // If it matches a command
-          const args = message.content
-            .substring(options.prefix.length + 1 + name.length)
-            .split(' ') // Make the args array
-          commands[name](message, args) // Run the command!
-        }
-      })
-    }
-  })
+            const args = message.content
+              .substring(options.prefix.length + 1 + name.length)
+              .split(' ') // Make the args array
+            commands[name](message, args) // Run the command!
+          }
+        })
+      }
+    })
   }
 
   remove () {
